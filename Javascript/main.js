41c6ef624db90a41c6164274b2bee02421adfe05
Nicholas-Lawson <68287060+Nicholas-Lawson@users.noreply.github.com>
2022-09-25 20:06:18 -0400

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,33 @@
+var counter = 0;
+
+function widthFlex()
+{
+    if(counter % 2 == 0)
+    {
+        $( ".option" ).css("border-right", "none");
+        $( ".option-box" ).css("width", "63%");
+    }
+    else
+    {
+        $( ".option" ).css("border-right", "1px solid black");
+        $( ".option-box" ).css("width", "fit-content");
+    }
+    counter++;
+}
+
+$(function()
+{
+    $( "a[id='Exp']" ).on("click", function()
+    {
+        $( "div[class='option res'], div[class='option about']" ).toggle("slow");
+        $( "div[class='exDesc']").toggle("slow");
+        $( "body" ).toggleClass( "body-desc-toggle" );
+    });
+
+    $( "a[id='Abt']" ).on("click", function()
+    {
+        $( "div[class='option res'], div[class='option ex']" ).toggle("slow");
+        $( "div[class='abtDesc']").toggle("slow");
+        $( "body" ).toggleClass( "body-desc-toggle" );
+    });
+});
